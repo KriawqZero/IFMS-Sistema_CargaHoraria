d50577b32dcee5c8cf92bee8cbe81792e91c88e8
ndavisilvan@gmail.com <ndavisilvan@gmail.com>
2025-01-10 23:04:12 -0400

    Incio Index Professor

--- resources/views/_layouts/sidebarprofessor.blade.php
+++ resources/views/_layouts/sidebarprofessor.blade.php
@@ -1,56 +1,46 @@
 <div x-cloak :class="sidebarOpen ? 'block' : 'hidden'" @click="sidebarOpen = false"
-  class="fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden"></div>
+    class="fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden"></div>
 
 <div x-cloak :class="sidebarOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
-  class="fixed bg-slate-800 inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform lg:translate-x-0 lg:static lg:inset-0">
-  <div class="flex items-center justify-center mt-8">
-    <div class="flex items-center">
-      <img href="{{ route('professor.dashboard') }}" class="mt-5 px-6" src="{{ asset('images/SISCO_1.png') }}" />
+    class="fixed bg-slate-800 inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform lg:translate-x-0 lg:static lg:inset-0">
+    <div class="flex items-center justify-center mt-8">
+        <div class="flex items-center">
+            <img href="{{ route('professor.dashboard') }}" class="mt-5 px-6" src="{{ asset('images/SISCO_1.png') }}" />
+        </div>
     </div>
-  </div>
 
-  <nav class="mt-10 text-slate-200">
-    <!-- Visão Geral -->
-    <a class="flex items-center px-6 py-2 mt-4 {{ request()->routeIs('professor.dashboard') ? 'bg-gray-700 bg-opacity-25' : 'hover:bg-opacity-25 hover:bg-gray-700 hover:text-gray-100' }}"
-      href="{{ route('professor.dashboard') }}">
-      <svg class="w-8 h-8" viewBox="0 0 18 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path d="M2.5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-        <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z" />
-      </svg>
-      <span class="mx-3">Visão Geral</span>
-    </a>
+    <nav class="mt-10 text-slate-200">
+        <!-- Visão Geral -->
+        <a class="flex items-center px-6 py-2 mt-4 {{ request()->routeIs('professor.dashboard') ? 'bg-gray-700 bg-opacity-25' : 'hover:bg-opacity-25 hover:bg-gray-700 hover:text-gray-100' }}"
+            href="{{ route('professor.dashboard') }}">
+            <svg class="w-8 h-8" viewBox="0 0 18 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+               <path d="M4 11a1 1 0 1 1 2 0v1a1 1 0 1 1-2 0zm6-4a1 1 0 1 1 2 0v5a1 1 0 1 1-2 0zM7 9a1 1 0 0 1 2 0v3a1 1 0 1 1-2 0z"/>
+  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
+  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
+            </svg>
+            <span class="mx-3">Visão Geral  </span>
+        </a>
 
-    <!-- Validar Certificados -->
-    <a class="flex items-center px-6 py-2 mt-4 {{ request()->routeIs('professor.validar') ? 'bg-gray-700 bg-opacity-25' : 'hover:bg-opacity-25 hover:bg-gray-700 hover:text-gray-100' }}"
-      href="">
-      <svg class="w-8 h-8" viewBox="0 0 18 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z" />
-        <path d="M5 10.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5z" />
-      </svg>
-      <span class="mx-3">Validar Certificados</span>
-    </a>
+        <!-- Validar Certificados -->
+        <a class="flex items-center px-6 py-2 mt-4 {{ request()->routeIs('professor.validar') ? 'bg-gray-700 bg-opacity-25' : 'hover:bg-opacity-25 hover:bg-gray-700 hover:text-gray-100' }}"
+            href="">
+            <svg class="w-8 h-8" viewBox="0 0 18 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z"/>
+  <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0"/>
+            </svg>
+            <span class="mx-3">Validar Certificados</span>
+        </a>
 
-    <!-- Alunos -->
-    <a class="flex items-center px-6 py-2 mt-4 {{ request()->routeIs('professor.alunos') ? 'bg-gray-700 bg-opacity-25' : 'hover:bg-opacity-25 hover:bg-gray-700 hover:text-gray-100' }}"
-      href="">
-      <svg class="w-8 h-8" viewBox="0 0 18 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path d="M4 1a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm1 7a4 4 0 1 1 8 0H5z" />
-      </svg>
-      <span class="mx-3">Alunos</span>
-    </a>
+        <!-- Alunos -->
+        <a class="flex items-center px-6 py-2 mt-4 {{ request()->routeIs('professor.alunos') ? 'bg-gray-700 bg-opacity-25' : 'hover:bg-opacity-25 hover:bg-gray-700 hover:text-gray-100' }}"
+            href="">
+            <svg class="w-8 h-8" viewBox="0 0 18 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+  <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+            </svg>
+            <span class="mx-3">Alunos</span>
+        </a>
 
-    <!-- Resumo da Turma -->
-    <a class="flex items-center px-6 py-2 mt-4 {{ request()->routeIs('professor.resumo') ? 'bg-gray-700 bg-opacity-25' : 'hover:bg-opacity-25 hover:bg-gray-700 hover:text-gray-100' }}"
-      href="">
-      <svg class="w-8 h-8" viewBox="0 0 18 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path d="M9 0a9 9 0 1 0 9 9A9 9 0 0 0 9 0zm0 2a7 7 0 1 1-7 7A7 7 0 0 1 9 2z" />
-        <path d="M8 5a1 1 0 0 1 1-1h1v7a1 1 0 0 1-2 0z" />
-      </svg>
-      <span class="mx-3">Resumo da Turma</span>
-    </a>
-  </nav>
 
-  <div class="flex justify-center">
-    <img src="{{ asset('svg/ifms-cb-logo.svg') }}" class="absolute bottom-0 my-6 h-10 mx-6" />
-  </div>
+    </nav>
 </div>
--- resources/views/professor/index.blade.php
+++ resources/views/professor/index.blade.php
@@ -1,23 +1,191 @@
 @extends('_layouts.master')
 
 @section('body')
-  <div class="mt-4">
-    <div class="flex flex-wrap -mx-6">
-      <div class="w-full px-6 sm:w-1/2 xl:w-1/4 sm:mt-1">
-        <div class="flex items-center px-5 py-6 shadow-xl rounded-3xl bg-white">
-          <div class="p-3 rounded-full bg-red-600 bg-opacity-75">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" fill="white" viewbox="0 0 16 16">
-              <path
-                d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5M4.285 9.567a.5.5 0 0 1 .683.183A3.5 3.5 0 0 0 8 11.5a3.5 3.5 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8" />
-            </svg>
-          </div>
+    <div class="mt-4">
+        <div class="flex flex-wrap -mx-6">
+            <div class="w-full px-6 sm:w-1/2 xl:w-1/4 sm:mt-1">
+                <div class="flex items-center px-5 py-6 shadow-xl rounded-3xl bg-white">
+                    <div class="p-3 rounded-full bg-red-600 bg-opacity-75">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" fill="white" viewbox="0 0 16 16">
+                            <path
+                                d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5M4.285 9.567a.5.5 0 0 1 .683.183A3.5 3.5 0 0 0 8 11.5a3.5 3.5 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8" />
+                        </svg>
+                    </div>
 
-          <div class="mx-5">
-            <h4 class="text-2xl font-semibold text-gray-700">{{ ucfirst(strtok($professor->nome, ' ')) }}</h4>
-            <div class="text-gray-500">Professor(a)</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-@endsection
+                    <div class="mx-5">
+                        <h4 class="text-2xl font-semibold text-gray-700">{{ ucfirst(strtok($professor->nome, ' ')) }}</h4>
+                        <div class="text-gray-500">Professor(a)</div>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Card das Turmas -->
+            <div class="bg-white shadow-lg rounded-2xl p-8">
+                <h3 class="text-3xl font-bold text-gray-800 mb-6 text-center">Turmas Designadas</h3>
+                <!-- Lista de Turmas -->
+                <div class="space-y-6">
+                    <!-- Turma 1 -->
+                    <div
+                        class="flex items-center bg-blue-100 shadow-md p-5 rounded-xl transition-transform transform hover:scale-105">
+                        <div class="flex items-center justify-center bg-green-200 rounded-full w-14 h-14 mr-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-green-700" fill="currentColor"
+                                viewBox="0 0 16 16">
+                                <path d="M4 4h8v1H4V4zm0 3h8v1H4V7zm0 3h8v1H4v-1zm0 3h8v1H4v-1z" />
+                            </svg>
+                        </div>
+                        <span class="text-gray-900 text-xl font-semibold">Turma 20210</span>
+                    </div>
+                    <!-- Turma 2 -->
+                    <div
+                        class="flex items-center bg-blue-100 shadow-md p-5 rounded-xl transition-transform transform hover:scale-105">
+                        <div class="flex items-center justify-center bg-green-200 rounded-full w-14 h-14 mr-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-green-700" fill="currentColor"
+                                viewBox="0 0 16 16">
+                                <path d="M4 4h8v1H4V4zm0 3h8v1H4V7zm0 3h8v1H4v-1zm0 3h8v1H4v-1z" />
+                            </svg>
+                        </div>
+                        <span class="text-gray-900 text-xl font-semibold">Turma 1079</span>
+                    </div>
+                    <!-- Turma 3 -->
+                    <div
+                        class="flex items-center bg-blue-100 shadow-md p-5 rounded-xl transition-transform transform hover:scale-105">
+                        <div class="flex items-center justify-center bg-green-200 rounded-full w-14 h-14 mr-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-green-700" fill="currentColor"
+                                viewBox="0 0 16 16">
+                                <path d="M4 4h8v1H4V4zm0 3h8v1H4V7zm0 3h8v1H4v-1zm0 3h8v1H4v-1z" />
+                            </svg>
+                        </div>
+                        <span class="text-gray-900 text-xl font-semibold">Turma 2079</span>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Table Certificados -->
+            <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+                <table class="min-w-full">
+                    <thead>
+                        <tr>
+                            <th
+                                class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                Certificados Enviados</th>
+                            <th
+                                class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                Data</th>
+                            <th
+                                class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                Aluno</th>
+                            <th
+                                class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                Status</th>
+                            <th class="px-6 py-3 border-b border-gray-200 bg-gray-100"></th>
+                        </tr>
+                    </thead>
+
+                    <tbody class="bg-white">
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 h-10 w-10">
+                                        <img class="h-10 w-10 rounded-full"
+                                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                            alt="" />
+                                    </div>
+
+                                    <div class="ml-4">
+                                        <div class="text-sm leading-5 font-medium text-gray-900">John Doe</div>
+                                        <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
+                                <div class="text-sm leading-5 text-gray-500">Web dev</div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <span
+                                    class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
+                            </td>
+
+                            <td
+                                class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                Owner</td>
+
+                            <td
+                                class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 h-10 w-10">
+                                        <img class="h-10 w-10 rounded-full"
+                                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                            alt="" />
+                                    </div>
+
+                                    <div class="ml-4">
+                                        <div class="text-sm leading-5 font-medium text-gray-900">John Doe</div>
+                                        <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
+                                <div class="text-sm leading-5 text-gray-500">Web dev</div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <span
+                                    class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
+                            </td>
+
+                            <td
+                                class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                Owner</td>
+
+                            <td
+                                class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 h-10 w-10">
+                                        <img class="h-10 w-10 rounded-full"
+                                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                            alt="" />
+                                    </div>
+
+                                    <div class="ml-4">
+                                        <div class="text-sm leading-5 font-medium text-gray-900">John Doe</div>
+                                        <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
+                                <div class="text-sm leading-5 text-gray-500">Web dev</div>
+                            </td>
+
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <span
+                                    class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
+                            </td>
+
+                            <td
+                                class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                                Owner</td>
+
+                            <td
+                                class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                            </td>
+                        </tr>
+                    @endsection
