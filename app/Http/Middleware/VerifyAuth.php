c27c2765ad97640cbbabaee2b147e37ece5b6252
ifmscb <ifmscb@laboratorio.ifms.br>
2025-02-03 13:09:43 -0400

    Corrigido bug de middleware

--- app/Http/Middleware/VerifyAuth.php
+++ app/Http/Middleware/VerifyAuth.php
@@ -18,7 +18,7 @@ class VerifyAuth {
     public function handle(Request $request, Closure $next, string $guard): Response {
         // Obtém os guards configurados no sistema.
         $guards = array_keys(config('auth.guards'));
-        if ($guard === "all") {
+        if ($guard == "all") {
             foreach ($guards as $specificGuard) {
                 // Se algum guard estiver autenticado, segue o fluxo.
                 if (auth($specificGuard)->check()) {
@@ -32,7 +32,7 @@ class VerifyAuth {
                 ->withErrors(['message' => 'Você precisa estar autenticado para acessar esta página.']);
         }
 
-        if($guard === "none") {
+        else if($guard == "none") {
             foreach($guards as $specificGuard) {
                 if(auth($specificGuard)->check()) {
                     return redirect()
@@ -40,6 +40,25 @@ class VerifyAuth {
                         ->withErrors('Você já está autenticado.');
                 }
             }
+        }
+
+        // Verifica se o usuário está autenticado pelo guard especificado.
+        else if (auth($guard)->check()) {
+            // Redireciona caso já esteja autenticado e tente acessar a rota de login.
+            if ($request->routeIs("$guard.login")) {
+                return redirect()
+                    ->route("$guard.dashboard")
+                    ->withErrors(['message' => 'Você já está autenticado.']);
+            }
+
+            return $next($request);
+        }
+
+        // Redireciona para o login se não estiver autenticado.
+        else {
+            return redirect()
+                ->route("aluno.login")
+                ->withErrors(['message' => 'Você precisa estar autenticado para acessar esta página.']);
         }
 
         return $next($request);
